f8f67459790f60b3a4bd4b8e1c166a5962ba3c69
Filippo Peruzzo <88912683+Fperuzzo@users.noreply.github.com>
2024-08-02 10:02:05 -0300

    Login com Google implementado

--- VidaAdulta/login.js
+++ VidaAdulta/login.js
@@ -41,3 +41,18 @@ function acceptCookies() {
     // Aqui você pode implementar a lógica para recusar os cookies
     document.getElementById('cookieBanner').style.display = 'none';
   }
+    // Integração de Login com Google 
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
